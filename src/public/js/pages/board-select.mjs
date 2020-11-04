ac16c9fe02f8b46ac78acb8dab14319a1780941e
Aurel Hudec <hudec117@gmail.com>
2020-11-04 20:32:04 +0000

    Menu and board selection UI first iteration

--- src/public/js/pages/board-select.mjs
+++ src/public/js/pages/board-select.mjs
@@ -1,8 +1,29 @@
 export default {
     name: 'BoardSelect',
     template: /*html*/`
-        <div>
-            Board Select
+        <div class="text-center">
+            <div class="row my-4">
+                <div class="col">
+                    <h1 class="display-3">Tic-Tac-Toe</h1>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col">
+                    <button type="button" class="btn btn-primary">3x3 Board</button>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col">
+                    <button type="button" class="btn btn-primary">4x4 Board</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <button type="button" class="btn btn-secondary" v-on:click="onBackClick">
+                        <i class="fas fa-arrow-left"></i>
+                    </button>
+                </div>
+            </div>
         </div>
     `,
     data: function() {
@@ -12,5 +33,10 @@ export default {
     },
     mounted: function() {
         
+    },
+    methods: {
+        onBackClick: function() {
+            this.$store.commit('setPage', 'MainMenu');
+        }
     }
 };
